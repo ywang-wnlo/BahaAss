5db63c88799052c6e9c1537696bde33ea678b361
Yu Wang <ywang_wnlo@qq.com>
2022-11-22 11:03:46 +0800

    去除 template 文件，增加标题和集数信息

--- BahaAss.py
+++ BahaAss.py
@@ -3,6 +3,22 @@ import json
 
 import requests
 
+template_ass = '''[Script Info]
+Title: {}
+Original Script: BahaAss.py
+ScriptType: v4.00+
+Collisions: Normal
+PlayResY: 1080
+PlayResX: 1920
+WrapStyle: 0
+
+[v4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: DefaultStyle,Microsoft YaHei UI,50,&H80FFFFFF,&H80FFFFFF,&H80000000,&H80000000,-1,0,0,0,100,100,0,0,1,1,0,5,0,0,0,0
+
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text'''
+
 
 class BahaAss(object):
     def __init__(self):
@@ -13,18 +29,27 @@ class BahaAss(object):
         }
         self._sn_list = []
         self._sn_dict = {}
+        self._digits_num = None
+        self._title = None
 
     def _get_all_sn(self, base_url):
         base_response = requests.get(base_url, headers=self._headers)
         # with open('base.html', 'wb') as fp:
-        # fp.write(base_response.content)
+        #     fp.write(base_response.content)
         base_response.encoding = 'utf8'
 
         sn_list = re.findall(
             r'<a href=\"\?sn=(\d+)\">(\S+)</a>', base_response.text)
+        _max = 0
         for sn in sn_list:
             self._sn_list.append(sn[0])
             self._sn_dict[sn[0]] = int(sn[1])
+            _max = max(_max, int(sn[1]))
+        self._digits_num = len(str(_max))
+
+        h1_list = re.findall(
+            r'<h1>(\S+) \[\d+\]</h1>', base_response.text)
+        self._title = h1_list[0]
 
     def _get_danmu(self, sn) -> dict:
         danmu_url = 'https://ani.gamer.com.tw/ajax/danmuGet.php'
@@ -33,8 +58,8 @@ class BahaAss(object):
             danmu_url, danmu_data, headers=self._headers)
 
         danmu_json = json.loads(danmu_response.content)
-        with open('danmu.json', 'w') as fp:
-            json.dump(danmu_json, fp, ensure_ascii=False, indent='\t')
+        # with open('danmu.json', 'w') as fp:
+        #     json.dump(danmu_json, fp, ensure_ascii=False, indent='\t')
         return danmu_json
 
     def _time_str(self, time: int) -> str:
@@ -50,35 +75,31 @@ class BahaAss(object):
 
     # TODO 通过独立函数 get_pos_y 获取最佳 y 值
 
-    def _parse_danmu(self, danmu: dict) -> list:
-        danmu_list = []
-        mode_dict = {
-            0: 'ScrollStyle',
-            1: 'TopStyle',
-            2: 'BottomStyle'
-        }
-        with open('demo.ass', 'a') as fp:
+    def _parse_danmu(self, danmu: dict, sn: str):
+        title = '{}_{}'.format(self._title, str(
+            self._sn_dict[sn]).zfill(self._digits_num))
+        with open(f'{title}.ass', 'w') as fp:
+            # text 弹幕内容
+            # color #RGB值
+            # size 0->小 1->正常 2->大
+            # position 0->滚动 1->上方 2->下方
+            # time 0.1s
             y1 = 25
             y2 = 25
             y3 = 25
             t1 = 0
             t2 = 0
             t3 = 0
+            fp.write(template_ass.format(title))
             for one_dict in danmu:
-                # text 弹幕内容
-                # color #RGB值
-                # size 0->小 1->正常 2->大
-                # position 0->滚动 1->上方 2->下方
-                # time 0.1s
                 start_time = one_dict['time']
                 start_time_str = self._time_str(start_time)
                 end_time = start_time + 50  # 默认 5s
                 end_time_str = self._time_str(end_time)
-                mode = mode_dict[one_dict['position']]
                 # \pos(<x>, <y>)
                 # \move(<x1>, <y1>, <x2>, <y2>)
                 # \c&H<bbggrr>&
-                if mode == 'ScrollStyle':
+                if one_dict['position'] == 0:
                     if start_time < t1:
                         y1 += 50
                         y1 = y1 % 400
@@ -86,7 +107,7 @@ class BahaAss(object):
                         y1 = 25
                     t1 = end_time
                     style = f'{{\move(1920,{y1},0,{y1})'
-                elif mode == 'TopStyle':
+                elif one_dict['position'] == 1:
                     if start_time < t2:
                         y2 += 50
                         y2 = y2 % 400
@@ -103,18 +124,16 @@ class BahaAss(object):
                     t3 = end_time
                     style = f'{{\pos(960,{1080-y3})'
                 style += '\c&H' + one_dict['color'][1:] + '&}'
-                text = one_dict['text']
-                fp.write('Dialogue: 0,{},{},{},,0,0,0,,{}{}\n'.format(
-                    start_time_str, end_time_str, mode, style, text))
-
-        return danmu_list
+                text = one_dict['text'].strip()
+                fp.write('\nDialogue: 0,{},{},DefaultStyle,,0,0,0,,{}{}'.format(
+                    start_time_str, end_time_str, style, text))
 
     def run(self):
         base_url = 'https://ani.gamer.com.tw/animeVideo.php?sn=20219'
         self._get_all_sn(base_url)
         for sn in self._sn_list:
             danmu = self._get_danmu(sn)
-            self._parse_danmu(danmu)
+            self._parse_danmu(danmu, sn)
             break  # TODO
 
 
