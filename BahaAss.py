d5e6b03beacb0fa20b8e1d871964643b7014cb4f
Yu Wang <ywang_wnlo@qq.com>
2022-11-22 12:58:46 +0800

    将原本获取 pos 的逻辑独立成单个函数，方便后面优化

--- BahaAss.py
+++ BahaAss.py
@@ -32,6 +32,9 @@ class BahaAss(object):
         self._sn_dict = {}
         self._digits_num = None
         self._title = None
+        self._end_time_pos = [0, 0]
+        self._y_pos = [25, 25]
+        self._diff_time = 50  # 默认 5s
 
     def _get_all_sn(self, base_url):
         base_response = requests.get(base_url, headers=self._headers)
@@ -74,7 +77,24 @@ class BahaAss(object):
 
         return f'{hour_str}:{minute_str}:{second_str}'
 
-    # TODO 通过独立函数 get_pos_y 获取最佳 y 值
+    def _get_pos(self, start_time: int, position: int) -> str:
+        # TODO 优化 pos 逻辑
+        # \pos(<x>, <y>)
+        pos_str = '\pos(960,{})'
+        index = position - 1
+        
+        if start_time < self._end_time_pos[index]:
+            self._y_pos[index] += 50
+            self._y_pos[index] %= 500
+        else:
+            self._y_pos[index] = 25
+        self._end_time_pos[index] = start_time + self._diff_time
+
+        if position == 1:
+            pos_str = pos_str.format(self._y_pos[index])
+        else:
+            pos_str = pos_str.format(1080 - self._y_pos[index])
+        return pos_str
 
     def _parse_danmu(self, danmu: dict, sn: str):
         title = '{}_{}'.format(self._title, str(
@@ -86,47 +106,31 @@ class BahaAss(object):
             # position 0->滚动 1->上方 2->下方
             # time 0.1s
             y1 = 25
-            y2 = 25
-            y3 = 25
             t1 = 0
-            t2 = 0
-            t3 = 0
             fp.write(template_ass.format(title))
             for one_dict in danmu:
                 start_time = one_dict['time']
                 start_time_str = self._time_str(start_time)
-                end_time = start_time + 50  # 默认 5s
+                end_time = start_time + self._diff_time
                 end_time_str = self._time_str(end_time)
-                # \pos(<x>, <y>)
-                # \move(<x1>, <y1>, <x2>, <y2>)
+                position = one_dict['position']
+                color = one_dict['color'][1:]
                 # \c&H<bbggrr>&
-                if one_dict['position'] == 0:
+                color_str = '\c&H{}&'.format(color)
+                text = one_dict['text'].strip()
+                if position == 0:
+                    # \move(<x1>, <y1>, <x2>, <y2>)
                     if start_time < t1:
                         y1 += 50
                         y1 = y1 % 400
                     else:
                         y1 = 25
                     t1 = end_time
-                    style = f'{{\move(1920,{y1},0,{y1})'
-                elif one_dict['position'] == 1:
-                    if start_time < t2:
-                        y2 += 50
-                        y2 = y2 % 400
-                    else:
-                        y2 = 25
-                    t2 = end_time
-                    style = f'{{\pos(960,{y2})'
+                    style = f'\move(1920,{y1},0,{y1}){color_str}'
                 else:
-                    if start_time < t3:
-                        y3 += 50
-                        y3 = y3 % 400
-                    else:
-                        y3 = 25
-                    t3 = end_time
-                    style = f'{{\pos(960,{1080-y3})'
-                style += '\c&H' + one_dict['color'][1:] + '&}'
-                text = one_dict['text'].strip()
-                fp.write('\nDialogue: 0,{},{},DefaultStyle,,0,0,0,,{}{}'.format(
+                    pos_str = self._get_pos(start_time, position)
+                    style = f'{pos_str}{color_str}'
+                fp.write('\nDialogue: 0,{},{},DefaultStyle,,0,0,0,,{{{}}}{}'.format(
                     start_time_str, end_time_str, style, text))
 
     def run(self):
